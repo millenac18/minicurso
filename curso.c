8a6f6ccb3ea92826368a3f6e0efc8404baa0d0a9
millenac18 <santanamilena357@gmail.com>
2024-11-08 01:11:15 +0000

    fiz o curso

---
+++ curso.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main ()
+{
+    int b,A ,c;
+  printf("digite dois valores numerico:");  
+  scanf( "%d %d", &A,&b);
+c= A -b;
+printf(" A subtração deles é : %d",c);  
+}
---
+++ lista.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main ()
+{
+int num = 0;
+printf("digite um valor numerico:");
+scanf( "%d", &num);
+printf( " O valor digitado foi %d\n:" , num);
+
+
+
+
+
+
+}
+
